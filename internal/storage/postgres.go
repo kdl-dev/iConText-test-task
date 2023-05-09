eebca56431de00b39acac46c95742ea7ba7b89e7
kdl-dev <dmitriy.l.kozyrev@gmail.com>
2023-05-10 01:04:20 +0700

    Added reconnect for postgres and redis connection pools.

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -3,13 +3,19 @@ package storage
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kdl-dev/iConText-test-task/internal/config"
+	"github.com/kdl-dev/iConText-test-task/logger"
 )
 
 type PostgresPool struct {
-	Pool *pgxpool.Pool
+	Pool              *pgxpool.Pool
+	closeSignal       chan bool
+	succesCloseSignal chan bool
 }
 
 type PostgresOptions struct {
@@ -18,6 +24,31 @@ type PostgresOptions struct {
 	Host     string
 	DBName   string
 	SSLMode  string
+}
+
+var (
+	PostgresOpt *PostgresOptions
+	Postgres    *PostgresPool
+)
+
+func init() {
+	PostgresOpt = &PostgresOptions{
+		Role:     config.AppCfg.Postgres.Role,
+		Password: config.AppCfg.Postgres.Password,
+		Host:     fmt.Sprintf("%s:%s", config.AppCfg.Postgres.Host, config.AppCfg.Postgres.Port),
+		DBName:   config.AppCfg.Postgres.DBName,
+		SSLMode:  config.AppCfg.Postgres.SSLMode,
+	}
+
+	var err error
+	Postgres, err = NewPostgresPool(context.Background(), PostgresOpt)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err = Postgres.InitTables(os.Getenv("PG_INIT_TABLES_FILE_PATH")); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func NewPostgresPool(ctx context.Context, opt *PostgresOptions) (*PostgresPool, error) {
@@ -32,9 +63,17 @@ func NewPostgresPool(ctx context.Context, opt *PostgresOptions) (*PostgresPool, 
 		return nil, fmt.Errorf("postgres ping error: %w", err)
 	}
 
-	return &PostgresPool{
-		Pool: pool,
-	}, nil
+	logger.Log.Info(fmt.Sprintf("postgres run on %s", opt.Host))
+
+	pgPool := &PostgresPool{
+		Pool:              pool,
+		closeSignal:       make(chan bool, 1),
+		succesCloseSignal: make(chan bool, 1),
+	}
+
+	go pgPool.KeepAlive()
+
+	return pgPool, nil
 }
 
 func (p *PostgresPool) InitTables(sqlFile string) error {
@@ -49,4 +88,43 @@ func (p *PostgresPool) InitTables(sqlFile string) error {
 	}
 
 	return nil
+}
+
+func (pg *PostgresPool) KeepAlive() {
+	var err error
+	var isLost bool = false
+	for {
+		select {
+		case <-pg.closeSignal:
+			pg.Pool.Close()
+			logger.Log.Info("postgres connection pool success closed")
+			pg.succesCloseSignal <- true
+			return
+		default:
+			time.Sleep(time.Second * time.Duration(config.AppCfg.Postgres.KeepAlivePoolPeriod))
+
+			if err = pg.Pool.Ping(context.Background()); err == nil {
+				if isLost {
+					logger.Log.Info("postgres success reconnected")
+					isLost = false
+				}
+
+				continue
+			}
+
+			isLost = true
+			logger.Log.Error(fmt.Errorf("postgres ping error: %w", err).Error())
+			logger.Log.Warn("lost postgresql connection. Restoring...")
+
+			pg.Pool, err = pgxpool.New(context.Background(), pg.Pool.Config().ConnString())
+			if err != nil {
+				logger.Log.Error(fmt.Errorf("postgres pool create error: %w", err).Error())
+			}
+		}
+	}
+}
+
+func (pg *PostgresPool) Close() {
+	pg.closeSignal <- true
+	<-pg.succesCloseSignal
 }
--- internal/storage/redis.go
+++ internal/storage/redis.go
@@ -2,13 +2,47 @@ package storage
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/kdl-dev/iConText-test-task/internal/config"
+	"github.com/kdl-dev/iConText-test-task/logger"
 	"github.com/redis/go-redis/v9"
 )
 
 type RedisPool struct {
-	Pool *redis.Client
+	Pool               *redis.Client
+	closeSignal        chan bool
+	successCloseSignal chan bool
+}
+
+var (
+	redisHost *string // flag
+	redisPort *string // flag
+
+	RedisOpt *redis.Options
+	Redis    *RedisPool
+)
+
+func init() {
+	redisHost = flag.String("host", config.AppCfg.Redis.Host, "redis host")
+	redisPort = flag.String("port", config.AppCfg.Redis.Port, "redis port")
+	flag.Parse()
+
+	RedisOpt = &redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", *redisHost, *redisPort),
+		DB:       config.AppCfg.Redis.DB,
+		Username: config.AppCfg.Redis.User,
+		Password: config.AppCfg.Redis.Password,
+	}
+
+	var err error
+	Redis, err = NewRedisPool(context.Background(), RedisOpt)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func NewRedisPool(ctx context.Context, opt *redis.Options) (*RedisPool, error) {
@@ -19,7 +53,55 @@ func NewRedisPool(ctx context.Context, opt *redis.Options) (*RedisPool, error) {
 		return nil, fmt.Errorf("redis ping error: %w", err)
 	}
 
-	return &RedisPool{
-		Pool: redis.NewClient(opt),
-	}, nil
+	logger.Log.Info(fmt.Sprintf("redis run on %s", opt.Addr))
+
+	redisPool := &RedisPool{
+		Pool:               redis.NewClient(opt),
+		closeSignal:        make(chan bool, 1),
+		successCloseSignal: make(chan bool, 1),
+	}
+
+	go redisPool.KeepAlive()
+
+	return redisPool, nil
+}
+
+func (rds *RedisPool) KeepAlive() {
+	var err error
+	var isLost bool = false
+	var status *redis.StatusCmd
+	for {
+		select {
+		case <-rds.closeSignal:
+			if err = rds.Pool.Close(); err != nil {
+				logger.Log.Error(fmt.Errorf("redis connection pool close error: %w", err).Error())
+				return
+			}
+			logger.Log.Info("redis connection pool success closed")
+			rds.successCloseSignal <- true
+			return
+		default:
+			time.Sleep(time.Second * time.Duration(config.AppCfg.Redis.KeepAlivePoolPeriod))
+
+			status = rds.Pool.Ping(context.Background())
+			if status.Err() == nil {
+				if isLost {
+					logger.Log.Info("redis success reconnected")
+					isLost = false
+				}
+				continue
+			}
+
+			isLost = true
+			logger.Log.Warn("lost redis connection. Restoring...")
+			logger.Log.Error(fmt.Errorf("redis ping error: %w", status.Err()).Error())
+
+			rds.Pool = redis.NewClient(rds.Pool.Options())
+		}
+	}
+}
+
+func (rds *RedisPool) Close() {
+	rds.closeSignal <- true
+	<-rds.successCloseSignal
 }
