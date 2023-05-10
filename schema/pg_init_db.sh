7975af81b2f43dbc3ba08018a66c60c64bff5cf5
kdl-dev <dmitriy.l.kozyrev@gmail.com>
2023-05-10 21:13:13 +0700

    Added .gitignore and fixed schema/pg_init_db.sh.

--- schema/pg_init_db.sh
+++ schema/pg_init_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'icontext_test_task'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE icontext_test_task";`
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'icontext_test_task'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE icontext_test_task";
