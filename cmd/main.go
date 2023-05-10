5ce3ae519a0955b7eae166c63649df62aa87d83e
kdl-dev <dmitriy.l.kozyrev@gmail.com>
2023-05-11 02:52:36 +0700

    Put things in order.

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,105 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/kdl-dev/iConText-test-task/internal/config"
 	"github.com/kdl-dev/iConText-test-task/internal/handler"
+	"github.com/kdl-dev/iConText-test-task/internal/repository"
+	"github.com/kdl-dev/iConText-test-task/internal/service"
 	"github.com/kdl-dev/iConText-test-task/internal/storage"
 	"github.com/kdl-dev/iConText-test-task/logger"
 	"github.com/kdl-dev/iConText-test-task/server"
+	"github.com/redis/go-redis/v9"
 )
 
 var (
+	envConfigFilePath    = ".env"
+	loggerConfigFilePath = "config/logger.yaml"
+
 	httpServer *http.Server
 	appServer  *server.Server
+	Handlers   *handler.Handler
+	Router     *http.ServeMux
+	Context    = context.Background()
+	Services   *service.Service
+	Repo       *repository.Repository
+
+	PostgresOpt *storage.PostgresOptions
+	Postgres    *storage.PostgresPool
+	redisHost   *string // flag
+	redisPort   *string // flag
+
+	RedisOpt *redis.Options
+	Redis    *storage.RedisPool
 )
 
 func init() {
+	var err error
+	config.AppCfg = &config.App{}
+
+	if err := cleanenv.ReadConfig(envConfigFilePath, config.AppCfg); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cleanenv.ReadConfig(loggerConfigFilePath, &config.AppCfg.Logger); err != nil {
+		log.Fatal(err)
+	}
+
+	if logger.Log, err = logger.NewLogger(&config.AppCfg.Logger); err != nil {
+		log.Fatal(err)
+	}
+
+	redisHost = flag.String("host", config.AppCfg.Redis.Host, "redis host")
+	redisPort = flag.String("port", config.AppCfg.Redis.Port, "redis port")
+	flag.Parse()
+
+	RedisOpt = &redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", *redisHost, *redisPort),
+		DB:       config.AppCfg.Redis.DB,
+		Username: config.AppCfg.Redis.User,
+		Password: config.AppCfg.Redis.Password,
+	}
+
+	Redis, err = storage.NewRedisPool(context.Background(), RedisOpt)
+	if err != nil {
+		logger.Log.Fatal(err.Error())
+	}
+
+	PostgresOpt = &storage.PostgresOptions{
+		Role:     config.AppCfg.Postgres.Role,
+		Password: config.AppCfg.Postgres.Password,
+		Host:     fmt.Sprintf("%s:%s", config.AppCfg.Postgres.Host, config.AppCfg.Postgres.Port),
+		DBName:   config.AppCfg.Postgres.DBName,
+		SSLMode:  config.AppCfg.Postgres.SSLMode,
+	}
+
+	Postgres, err = storage.NewPostgresPool(context.Background(), PostgresOpt)
+	if err != nil {
+		logger.Log.Fatal(err.Error())
+	}
+
+	if err = Postgres.InitTables(os.Getenv("PG_INIT_TABLES_FILE_PATH")); err != nil {
+		logger.Log.Fatal(err.Error())
+	}
+
+	Repo = repository.NewRepository(Postgres, Redis)
+	Services = service.NewService(Repo)
+	Handlers = handler.NewHandler(Context, Services, logger.Log)
+	Router = http.NewServeMux()
+	Handlers.InitRoutes(Router)
+
 	httpServer = new(http.Server)
 	httpServer.Addr = fmt.Sprintf("%s:%s", config.AppCfg.Server.Addr, config.AppCfg.Server.Port)
-	httpServer.Handler = handler.Router
+	httpServer.Handler = Router
+
 }
 
 func main() {
@@ -57,6 +134,6 @@ func main() {
 
 func cleanUpAfterYourself() {
 	appServer.Shutdown(context.Background())
-	storage.Postgres.Close()
-	storage.Redis.Close()
+	Postgres.Close()
+	Redis.Close()
 }
